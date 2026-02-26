0decfb79a43d7f30d5d3d3fc4e0d04f553cdccaa
Jeffrey Girard <jeffrey3224@gmail.com>
2026-02-25 23:23:11 -0800

    added navbar menu partial

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,11 +2,11 @@ class LikesController < ApplicationController
   before_action :set_post
   def create
     @post.likes.create(user: current_user)
-    render turbo_stream: turbo_stream.replace("like-section-{@post.id}", partial: "posts/like_section", locals: { post: @post })
+    render turbo_stream: turbo_stream.replace("like-section-#{@post.id}", partial: "posts/like_section", locals: { post: @post })
   end
-  def create
+  def destroy
     @post.likes.find_by(user: current_user)&.destroy
-    render turbo_stream: turbo_stream.replace("like-section-{@post.id}", partial: "posts/like_section", locals: { post: @post })
+    render turbo_stream: turbo_stream.replace("like-section-#{@post.id}", partial: "posts/like_section", locals: { post: @post })
   end
 
   def set_post
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,22 @@ import "controllers"
 
 import "trix"
 import "@rails/actiontext"
+
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("dropdown-button")
+    const menu = document.getElementById("dropdown-menu");
+
+    button.addEventListener("click", function() {
+        if (menu.style.display === "block") {
+            menu.style.display = "none"
+        } else {
+            menu.style.display = "block"
+        }
+    })
+
+    window.addEventListener("click", function(event) {
+        if (!button.contains(event.target) && !menu.contains(event.target)) {
+            menu.style.display = "none"
+        }
+    })
+})
